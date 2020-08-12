92c3c8711ddf48c9820068e0a0cfcf888e142b28
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-08-11 18:25:21 -0700

    build: Add 3.6 to build matrix

--- rb/ketama.py
+++ rb/ketama.py
@@ -5,7 +5,7 @@ from bisect import bisect
 
 
 def md5_bytes(key):
-    return map(ord, hashlib.md5(key).digest())
+    return bytearray(hashlib.md5(key.encode("utf-8")).digest())
 
 
 class Ketama(object):
